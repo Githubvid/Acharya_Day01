3abea5bd9bcdc74ecba3075f1b1e3a3aa845635b
sai <sai@DESKTOP-51C54RL>
2022-06-19 11:05:06 +0530

    First commit Uploading all the files

---
+++ Acharya_Day01/src/com/acharya/demo/Simple.java
@@ -0,0 +1,10 @@
+package com.acharya.demo;
+
+public class Simple {
+	public static void main(String[]args) {
+		float f=(float) 10.5 ;
+		int a=(int)f;
+		System.out.println(f);
+		System.out.println(a);
+	}
+}
